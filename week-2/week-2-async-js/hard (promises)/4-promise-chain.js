211000a54c211b0b4ad9a3f034a0f6d319466f12
Bhargav <bhargavdash@gmail.com>
2025-03-12 16:39:17 +0530

    Completed async-js hard questions

--- week-2/week-2-async-js/hard (promises)/4-promise-chain.js
+++ week-2/week-2-async-js/hard (promises)/4-promise-chain.js
@@ -6,19 +6,28 @@
  */
 
 function wait1(t) {
-
+    return new Promise((resolve) => setTimeout(resolve, t*1000));
 }
 
 function wait2(t) {
-
+    return new Promise((resolve) => setTimeout(resolve, t*1000));
 }
 
 function wait3(t) {
-
+    return new Promise((resolve) => setTimeout(resolve, t*1000));
 }
-
+function chain(t1,t2,t3){
+    return wait1(t1).then(() => {
+        return wait2(t2).then(() => {
+            return wait3(t3);
+        })
+    })
+}
 function calculateTime(t1, t2, t3) {
-
+    let startTime = new Date().getTime();
+    return chain(t1,t2,t3).then(() => {
+        return (new Date().getTime() - startTime);
+    })
 }
 
 module.exports = calculateTime;
