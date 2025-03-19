a8f9ab8b0a51786157ad22311f0c9c484f812c8b
Bhargav <bhargavdash@gmail.com>
2025-03-19 13:58:52 +0530

    Deleted .env.example file

--- week-5/backend/routes/todo.js
+++ week-5/backend/routes/todo.js
@@ -2,9 +2,41 @@
 
 const { Router } = require('express');
 const router = Router();
+const { auth } = require('../middleware/user');
+const app = express();
 
-router.get('/get-todo', (req, res)=> {
-    res.send("Hello from todo/get-todo route");
+
+router.get('/healthy', (req, res)=> {
+    res.send("Hello from todos");
 })
+
+app.use(auth);
+
+// add create todo endpoint
+router.post('/create', async(req, res) =>{
+
+});
+
+// add get all todos endpoint 
+router.get('/get', async(req, res) =>{
+
+});
+
+// add get todo by id endpoint
+router.get('/get/:id', async(req, res) =>{
+
+});
+
+// add update todo endpoint
+router.put('/update/:id', async(req, res) =>{
+
+});
+
+// add delete todo endpoint
+router.delete('/delete/:id', async(req, res) =>{
+
+});
+
+
 
 module.exports = router;
