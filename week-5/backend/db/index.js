0a7003bbf785f21d839a0e3e0f317a1eabcb8b0c
Bhargav <bhargavdash@gmail.com>
2025-03-19 12:54:03 +0530

    Week-5 server and db setup done

--- week-5/backend/db/index.js
+++ week-5/backend/db/index.js
@@ -1,1 +1,28 @@
 //  start writing from here
+// include mongoose , schema and ObjectId
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+const ObjectId = Schema.ObjectId;
+
+// connect to mongoose in the main server file
+
+// create schemas
+const userSchema = new Schema({
+    username: {type: String, unique: true},
+    password: String
+})
+
+const todoSchema = new Schema({
+    title: String,
+    isDone: Boolean,
+    timestamp: String,
+    userId: ObjectId
+})
+
+const User = mongoose.model('users', userSchema);
+const Todo = mongoose.model('todos', todoSchema);
+
+module.exports = {
+    User,
+    Todo
+}
--- week-5/backend/index.js
+++ week-5/backend/index.js
@@ -1,1 +1,20 @@
 // start writing from here
+
+const express = require('express');
+const mongoose = require('mongoose');
+
+// connect to db
+mongoose.connect('mongodb+srv://bhargavdash:aI7fZBQ73k8Mdcul@cluster0.orzmy.mongodb.net/todo-app').then(()=>{
+    console.log("DB connected!!");
+})
+
+const app = express();
+
+// to parse json 
+app.use(express.json());
+
+app.get('/healthy', (req, res)=>{
+    res.send("I am healthy");
+})
+
+app.listen(3000, ()=> console.log("Server running on port 3000"));
