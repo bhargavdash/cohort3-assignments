29bcb2c9979bbefd4f1a2a2d0ae5a7ece29685d6
Bhargav <bhargavdash@gmail.com>
2025-03-19 12:44:12 +0530

    Week-5 Signup & SignIn frontend done

--- week-5/frontend/script.js
+++ week-5/frontend/script.js
@@ -1,1 +1,12 @@
-//  start from here
+document.getElementById("show-sign-in").addEventListener('click', () =>{
+    document.getElementById("signupId").style.display = "none";
+    document.getElementById("signinId").style.display = "block";
+})
+
+document.getElementById("show-sign-up").addEventListener('click', () =>{
+    document.getElementById("signinId").style.display = "none";
+    document.getElementById("signupId").style.display = "block";
+})
+
+let signedIn = false;
+
